66666e6b75e7b2a1332eea98a1cf242d2e716f32
rspivak <devnull@localhost>
2004-11-25 16:53:38 +0000

    Added unit test for 'replaceTextElementsByStyleName'.

--- tests/test_ooxmlhelpers.py
+++ tests/test_ooxmlhelpers.py
@@ -6,10 +6,19 @@ if __name__ == '__main__':
 
 import unittest
 from os.path import join, abspath, dirname
+import xml.dom
 import xml.dom.minidom
 from Products.CPSOOo.OOoDocbookDocument import iterDoc, toUnicode, \
-     replaceKeywords, replaceBiblio
+     replaceKeywords, replaceBiblio, replaceTextElementsByStyleName
 
+
+def docorder_iter_filter(node, filter_func, **kw):
+    if filter_func(node, **kw):
+        yield node
+    for child in node.childNodes:
+        for cn in docorder_iter_filter(child, filter_func, **kw):
+            yield cn
+    return
 
 class TestOOXMLHelpers(unittest.TestCase):
 
@@ -20,6 +29,13 @@ class TestOOXMLHelpers(unittest.TestCase):
 
     def _getOriginalXML(self):
         return self._getExpectedResult('orig.xml')
+
+    def _getElementsByStyleName(self, doc, style_name):
+        stylename_filter = lambda node, style_name='': \
+            (node.nodeType == xml.dom.Node.ELEMENT_NODE and
+             style_name == node.getAttribute('text:style-name'))
+        return list(docorder_iter_filter(doc, stylename_filter,
+                                         style_name=style_name))
 
     def test_replaceKeywords(self):
         doc = self._getOriginalXML()
@@ -38,6 +54,56 @@ class TestOOXMLHelpers(unittest.TestCase):
             }
         iterDoc(doc, replaceBiblio, iter_context)
         self.assertEqual(doc.toxml(), expected.toxml(), 'Results do not match')
+
+    def test_replaceTextElementsByStyleName(self):
+        doc = self._getOriginalXML()
+        iter_context = {
+            'textnodes': (
+                ('text:p', 'text:style-name', 'Title',
+                 toUnicode('The Title')),
+                ('text:p', 'text:style-name', 'Abstract',
+                 toUnicode('The Abstract')),
+                ('text:p', 'text:style-name', 'Bibliocoverage',
+                 toUnicode('The Bibliocoverage')),
+                ('text:p', 'text:style-name', 'Bibliosource',
+                 toUnicode('The Bibliosource')),
+                ),
+            }
+        title_elements = self._getElementsByStyleName(doc, 'Title')
+        self.assertEqual(len(title_elements), 8)
+
+        abstract_elements = self._getElementsByStyleName(doc, 'Abstract')
+        self.assertEqual(len(abstract_elements), 1)
+
+        bibliocoverage_elements = self._getElementsByStyleName(doc,
+                                                               'Bibliocoverage')
+        self.assertEqual(len(bibliocoverage_elements), 1)
+
+        bibliosource_elements = self._getElementsByStyleName(doc,
+                                                             'Bibliosource')
+        self.assertEqual(len(bibliosource_elements), 1)
+
+        iterDoc(doc, replaceTextElementsByStyleName, iter_context)
+
+        title_elements = self._getElementsByStyleName(doc, 'Title')
+        self.assertEqual(len(title_elements), 1)
+
+        node = self._getElementsByStyleName(doc, 'Title')[0]
+        text_val = node.firstChild.nodeValue
+        self.assertEqual(text_val, 'The Title')
+
+        node = self._getElementsByStyleName(doc, 'Abstract')[0]
+        text_val = node.firstChild.nodeValue
+        self.assertEqual(text_val, 'The Abstract')
+
+        node = self._getElementsByStyleName(doc, 'Bibliocoverage')[0]
+        text_val = node.firstChild.nodeValue
+        self.assertEqual(text_val, 'The Bibliocoverage')
+
+        node = self._getElementsByStyleName(doc, 'Bibliosource')[0]
+        text_val = node.firstChild.nodeValue
+        self.assertEqual(text_val, 'The Bibliosource')
+
 
 def test_suite():
     suite = unittest.TestSuite()
